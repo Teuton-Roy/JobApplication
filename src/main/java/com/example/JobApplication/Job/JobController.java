6bc605b627524988f4b8ca69a12ece55f4f1ef75
Teuton Roy <popeyehk404@gmail.com>
2024-10-23 23:24:48 +0530

    Create JobController and Job class and define them.

--- src/main/java/com/example/JobApplication/Job/JobController.java
+++ src/main/java/com/example/JobApplication/Job/JobController.java
@@ -1,4 +1,13 @@
 package com.example.JobApplication.Job;
 
+import java.util.*;
 public class JobController {
+
+    //Defining job controller here...
+
+    private List<job> jobs= new ArrayList<>(); //List of jobs
+    public List<job> findAll(){ //this the endpoint we are creating here which return all the jobs
+        return jobs;
+    }
+
 }
---
+++ src/main/java/com/example/JobApplication/Job/job.java
@@ -0,0 +1,75 @@
+package com.example.JobApplication.Job;
+
+public class job {
+
+    //Need to define what we need in this class, and what all info we need about the job...
+
+
+    //fields for every jobs
+    private Long id; //unique id for each job
+    private String title;
+    private String description;
+    private String minSalary;
+    private String maxSalary;
+    private String location;
+
+    //constructor
+    public job(Long id, String title, String description, String minSalary, String maxSalary, String location){
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.minSalary = minSalary;
+        this.maxSalary = maxSalary;
+        this.location = location;
+    }
+
+    //Getter and Setter
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getMinSalary() {
+        return minSalary;
+    }
+
+    public void setMinSalary(String minSalary) {
+        this.minSalary = minSalary;
+    }
+
+    public String getMaxSalary() {
+        return maxSalary;
+    }
+
+    public void setMaxSalary(String maxSalary) {
+        this.maxSalary = maxSalary;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
