e9edcb3c360f3c289050cd15d9922a8f92d4d6fb
Teuton Roy <popeyehk404@gmail.com>
2024-10-23 23:28:14 +0530

    add get method to JobController to fetch all the jobs...

--- src/main/java/com/example/JobApplication/Job/JobController.java
+++ src/main/java/com/example/JobApplication/Job/JobController.java
@@ -1,11 +1,15 @@
 package com.example.JobApplication.Job;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 import java.util.*;
 public class JobController {
 
     //Defining job controller here...
 
     private List<job> jobs= new ArrayList<>(); //List of jobs
+
+    @GetMapping("/jobs")
     public List<job> findAll(){ //this the endpoint we are creating here which return all the jobs
         return jobs;
     }
