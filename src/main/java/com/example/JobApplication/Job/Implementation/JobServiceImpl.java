fedf55a1fe9a475527ca64b5293823175df16d4a
Teuton Roy <popeyehk404@gmail.com>
2024-10-24 19:01:37 +0530

    # add @Service annotation to jobservice implemetation to tell that this class is a service # create contructor of jobservice at controller

--- src/main/java/com/example/JobApplication/Job/Implementation/JobServiceImpl.java
+++ src/main/java/com/example/JobApplication/Job/Implementation/JobServiceImpl.java
@@ -2,10 +2,12 @@ package com.example.JobApplication.Job.Implementation;
 
 import com.example.JobApplication.Job.Job;
 import com.example.JobApplication.Job.JobService;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service                                                                   //It tells that this class is a service
 public class JobServiceImpl implements JobService {
     private List<Job> jobs= new ArrayList<>();
     @Override
--- src/main/java/com/example/JobApplication/Job/JobController.java
+++ src/main/java/com/example/JobApplication/Job/JobController.java
@@ -12,19 +12,26 @@ public class JobController {
 
     //Defining job controller here...
 
-    private List<Job> jobs= new ArrayList<>(); //List of jobs
+//    private List<Job> jobs= new ArrayList<>(); //List of jobs
+
+    private JobService jobservice;  //job_service will be available at runtime, and inject it to job_controller
+
+    //constructor
+    public JobController(JobService jobservice) {
+        this.jobservice = jobservice;
+    }
 
     //Get list of jobs
     @GetMapping("/jobs")
     public List<Job> findAll(){ //this the endpoint we are creating here which return all the jobs
-        return jobs;
+        return jobservice.findall();
     }
 
     //Create a new job
     @PostMapping("/jobs")
     public String CreateJob(@RequestBody Job job){
          //this the endpoint we are creating here which return all the jobs
-        jobs.add(job);
+        jobservice.createJob(job);
         return "Job added successfully";
     }
 }
