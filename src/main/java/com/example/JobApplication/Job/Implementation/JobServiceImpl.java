02835d7cce0b6ae096925a899598a77fb95acd90
Teuton Roy <popeyehk404@gmail.com>
2024-10-24 18:46:22 +0530

    Define JobService Interface and Implement JobService class

--- src/main/java/com/example/JobApplication/Job/Implementation/JobServiceImpl.java
+++ src/main/java/com/example/JobApplication/Job/Implementation/JobServiceImpl.java
@@ -1,5 +1,20 @@
 package com.example.JobApplication.Job.Implementation;
 
-public class JobServiceImpl {
+import com.example.JobApplication.Job.Job;
+import com.example.JobApplication.Job.JobService;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class JobServiceImpl implements JobService {
+    private List<Job> jobs= new ArrayList<>();
+    @Override
+    public List<Job> findall() {
+        return jobs;
+    }
+
+    @Override
+    public void createJob(Job job) {
+        jobs.add(job);
+    }
 }
--- src/main/java/com/example/JobApplication/Job/JobService.java
+++ src/main/java/com/example/JobApplication/Job/JobService.java
@@ -1,8 +1,12 @@
 package com.example.JobApplication.Job;
 
+import java.util.*;
 
 //Created this as an Interface to promote loss coupling.
 //Loss Coupling: Loose coupling is a design principle that aims to reduce the dependencies between different components or modules of a software system.
 // Why we use? Because, have some modularity in my code-base, so I can separate the interface and the implementation.
 public interface JobService {
+    //defining methods here...
+    List<Job>findall();
+    void createJob(Job job);
 }
