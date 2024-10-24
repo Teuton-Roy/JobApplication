f06c727d7080f799d254ce41051dea1eab9f574d
Teuton Roy <popeyehk404@gmail.com>
2024-10-24 18:38:41 +0530

    Created jobservice interface and implemantation.

---
+++ src/main/java/com/example/JobApplication/Job/Implementation/JobServiceImpl.java
@@ -0,0 +1,5 @@
+package com.example.JobApplication.Job.Implementation;
+
+public class JobServiceImpl {
+
+}
---
+++ src/main/java/com/example/JobApplication/Job/JobService.java
@@ -0,0 +1,8 @@
+package com.example.JobApplication.Job;
+
+
+//Created this as an Interface to promote loss coupling.
+//Loss Coupling: Loose coupling is a design principle that aims to reduce the dependencies between different components or modules of a software system.
+// Why we use? Because, have some modularity in my code-base, so I can separate the interface and the implementation.
+public interface JobService {
+}
