3187bc08a8beffdbfcdf8353c4f61bc0a492376d
Teuton Roy <popeyehk404@gmail.com>
2024-10-25 23:08:53 +0530

    add validation

--- src/main/java/com/example/JobApplication/Job/JobController.java
+++ src/main/java/com/example/JobApplication/Job/JobController.java
@@ -34,13 +34,24 @@ public class JobController {
 
     //Get specific job by ID
     @GetMapping("/jobsById/{id}") // {} -> dynamic, id -> variable name
-    public Job getJobById(@PathVariable Long id){
+    public Job getJobById(@PathVariable Long id) {
 
         //public Job, why? Because fetching a particular job, this will return a Job Object.
         //@PathVariable ->  this is used to extract the value from the URL.
 
         Job job = jobservice.getJobById(id);
-        return job;
+
+        //add validation, if given id is not present then show this dummy job
+        if (job != null){
+            return job;
+        }
+        return new Job(0L,
+                "dummy job",
+                "Their no job associated with this id",
+                "000",
+                "000",
+                "India"
+        );
     }
 }
 
