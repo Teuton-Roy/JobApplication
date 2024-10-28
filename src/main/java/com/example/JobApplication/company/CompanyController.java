efd38b9b83f885ebfdd6bf57d811ae906dabccff
Teuton Roy <popeyehk404@gmail.com>
2024-10-28 22:48:27 +0530

    - define the companyController.. - create getallCompany Method and use @GetMapping

--- src/main/java/com/example/JobApplication/company/CompanyController.java
+++ src/main/java/com/example/JobApplication/company/CompanyController.java
@@ -1,4 +1,28 @@
 package com.example.JobApplication.company;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.*;
+
+@RestController
+@RequestMapping("/companies")
 public class CompanyController {
+
+    //create object of companyService because controller will use the service object to work with company.
+
+    private CompanyService companyService;
+
+    public CompanyController(CompanyService companyService) {
+        this.companyService = companyService;
+    }
+
+    //Get all the companies
+    @GetMapping
+    public List<Company> getAllCompanies(){
+        return companyService.getallCompanies();
+    }
+
+
 }
