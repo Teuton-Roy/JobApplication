70c98521f5f3508b9ccf8d7383f6981b85ad6d93
Teuton Roy <popeyehk404@gmail.com>
2024-10-28 22:30:07 +0530

    - Define Company Entity

--- src/main/java/com/example/JobApplication/company/Company.java
+++ src/main/java/com/example/JobApplication/company/Company.java
@@ -1,4 +1,56 @@
 package com.example.JobApplication.company;
+import com.example.JobApplication.Job.Job;
 
+import jakarta.persistence.*;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
+import javax.persistence.GenerationType;
+import java.util.List;
+
+
+@Entity
 public class Company {
+
+    //Defining Company entity
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+
+
+    //Constructor
+    public Company() {
+
+    }
+
+    //map every company to list of jobs
+    @OneToMany //this indicates that one company has many jobs opening
+    private List<Job> jobs;
+    //private List<Review> reviews; later on...
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
