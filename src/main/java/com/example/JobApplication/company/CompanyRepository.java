81083e90de30798415c87841e88207bd215ec0ed
Teuton Roy <popeyehk404@gmail.com>
2024-10-28 22:32:15 +0530

    - set CompanyRepository

--- src/main/java/com/example/JobApplication/company/CompanyRepository.java
+++ src/main/java/com/example/JobApplication/company/CompanyRepository.java
@@ -1,4 +1,6 @@
 package com.example.JobApplication.company;
 
-public interface CompanyRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CompanyRepository extends JpaRepository<Company, Long> {
 }
